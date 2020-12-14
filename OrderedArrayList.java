54e7ba8e8552a36ceb11571cdea9589417ab24db
srinathstuycs <smahankali10@stuy.edu>
2020-12-14 05:29:50 -0500

    Overrode set()

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -38,4 +38,14 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
   public void add(int index, T element) {
     this.add(element);
   }
+
+  public T set(int index, T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Element null not allowed.");
+    }
+    T removed = this.get(index);
+    this.remove(index);
+    this.add(element);
+    return removed;
+  }
  }
