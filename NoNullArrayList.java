edd7244fc3fc10e30743842581c2163bda59c725
srinathstuycs <smahankali10@stuy.edu>
2020-12-14 03:58:32 -0500

    Overrode add()

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -8,4 +8,11 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList(int startingCapacity) {
     super(startingCapacity);
   }
+
+  public boolean add(T element) {
+    if (element == null) {
+      throw new IllegalArgumentException("Element null not allowed.");
+    }
+    return super.add(element);
+  }
 }
