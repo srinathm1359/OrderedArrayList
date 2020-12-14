3085a49c42d3c0f906f6886228d61ccaca8fcc8d
srinathstuycs <smahankali10@stuy.edu>
2020-12-14 05:24:39 -0500

    Overrode add()

--- OrderedArrayList.java
+++ OrderedArrayList.java
@@ -9,4 +9,29 @@ public class OrderedArrayList<T extends Comparable<T>> extends NoNullArrayList<T
     super(startingCapacity);
   }
 
+  public boolean add(T element) {
+    //Taking care of the null case
+    if (element == null) {
+      throw new IllegalArgumentException("Element null not allowed.");
+    }
+    //Taking care of the empty list case
+    if (this.size() == 0) {
+      super.add(0, element);
+      return true;
+    }
+    //Finding the right index
+    int addAt = 0;
+    T current = this.get(addAt);
+    while (addAt < this.size()) {
+      current = this.get(addAt);
+      if (element.compareTo(current) <= 0) {
+        super.add(addAt, element);
+        return true;
+      }
+      addAt++;
+    }
+    //Finish adding
+    super.add(addAt, element);
+    return true;
+  }
  }
