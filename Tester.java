adb3c26e8490ac95f20266bef004c7072a869bb5
Srinath2 <arvindjunior@Srinath-Air.lan>
2020-12-14 03:28:54 -0500

    Tested default constructor for NoNullArrayList

--- Tester.java
+++ Tester.java
@@ -1,4 +1,10 @@
 public class Tester {
   public static void main(String[] args) {
+    //Testing NoNullArrayList default constructor
+    NoNullArrayList<Integer> nonull1 = new NoNullArrayList<Integer>();
+    for (int i = 0; i < 10; i++) {
+      nonull1.add(i);
+    }
+    System.out.println(nonull1);
   }
 }
