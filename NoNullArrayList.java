b3c0e6e5fc5f60f8a47edc3129e2e67b35c12f48
Srinath2 <arvindjunior@Srinath-Air.lan>
2020-12-14 03:37:49 -0500

    Added startingCapacity constructor

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -4,4 +4,8 @@ public class NoNullArrayList<T> extends ArrayList<T> {
   public NoNullArrayList() {
     super();
   }
+
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
 }
